3d47821a53cc73d889b14888fdb24c947b50a787
Mickle <putilovms@gmail.com>
2024-10-31 11:27:16 +0400

    added StringValidator class

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,6 +1,38 @@
 from validator import Validator
 
 
-def test_test():
-    validator = Validator()
-    assert validator.test() is None
+def test_new_schema():
+    v = Validator()
+    schema = v.string()
+    schema2 = v.string()
+    assert schema is not schema2
+
+
+def test_empty_string_is_valid():
+    v = Validator()
+    schema = v.string()
+    assert schema.is_valid('') == True
+    assert schema.is_valid(None) == True
+    assert schema.is_valid('what does the fox say') == True
+
+
+def test_string_required():
+    v = Validator()
+    schema = v.string()
+    schema2 = v.string()
+    schema.required()
+    assert schema.is_valid('') == False
+    assert schema.is_valid(None) == False
+    assert schema2.is_valid('') == True
+    assert schema.is_valid('hexlet') == True
+
+
+def test_string_contains():
+    v = Validator()
+    schema = v.string()
+    assert schema.contains('what').is_valid('what does the fox say') == True
+    assert schema.contains('whatthe').is_valid('what does the fox say') == False
+
+def test_string_length():
+    v = Validator()
+    assert v.string().min_len(10).min_len(4).is_valid('Hexlet') == True
--- validator/validator.py
+++ validator/validator.py
@@ -1,3 +1,31 @@
+class StringValidator():
+    def __init__(self):
+        self.required_flag = False
+        self.contains_string = None
+        self.min_lenght = None
+
+    def required(self):
+        self.required_flag = True
+        return self
+
+    def contains(self, string):
+        self.contains_string = string
+        return self
+
+    def is_valid(self, string):
+        if self.required_flag and not string:
+            return False
+        if self.contains_string:
+            return self.contains_string in string
+        if self.min_lenght is not None:
+            return len(string) >= self.min_lenght
+        return True
+
+    def min_len(self, min_lenght):
+        self.min_lenght = min_lenght
+        return self
+
+
 class Validator():
-    def test(self):
-        pass
+    def string(self):
+        return StringValidator()
